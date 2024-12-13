c83b1d556c7c2fe486f455c5e90a44b3f509f272
Manisorn <manisorn0803@gmail.com>
2024-12-13 16:58:55 +0700

    lab9_1.cpp

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -3,9 +3,16 @@
 using namespace std;
 
 int main(){	
+    double loan;
+    double rate;
+    double amount;
+    
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rate;
 	cout << "Enter amount you can pay per year: ";
+	cin >> amount;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -18,16 +25,23 @@ int main(){
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
+	int i = 1;
+	while(i != 0){
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
+	cout << setw(13) << left << i;
+	cout << setw(13) << left << loan;
+	cout << setw(13) << left << loan * (rate/100);
+	cout << setw(13) << left << loan + loan * (rate/100);
+	if(loan + loan * (rate/100) <= amount){
+	cout << setw(13) << left << loan + loan * (rate/100);
+	cout << setw(13) << left << "0.00";
+	}else{
+	    cout << setw(13) << left << amount;
+	    cout << setw(13) << left << loan + loan * (rate/100) - amount;
+	}
 	cout << "\n";	
-	
-	return 0;
+	if(loan + loan * (rate/100) <= amount) break;
+	loan = loan + loan * (rate/100) - amount;
+	i++;
+	}
 }
